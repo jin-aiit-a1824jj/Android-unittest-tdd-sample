55f33b34810786b3f5bd47d10980c55716c6d63f
a1824jj <a1824jj@aiit.ac.jp>
2020-04-03 16:21:53 +0900

    prepare unittest for LoginUserCaseSyncTest

--- app/src/androidTest/java/a1824jj/jp/ac/aiit/androidunittesttdd_sample/test_doubles_fundamentals/LoginUseCaseSyncTest.java
+++ app/src/androidTest/java/a1824jj/jp/ac/aiit/androidunittesttdd_sample/test_doubles_fundamentals/LoginUseCaseSyncTest.java
@@ -1,7 +1,46 @@
 package a1824jj.jp.ac.aiit.androidunittesttdd_sample.test_doubles_fundamentals;
 
+import android.accounts.NetworkErrorException;
+
+import org.junit.Before;
+
 import static org.junit.Assert.*;
 
 public class LoginUseCaseSyncTest {
 
+    LoginUseCaseSync SUT;
+
+    @Before
+    public void setup(){
+        SUT = new LoginUseCaseSync(new LoginHttpEndpointSyncTd(), new AuthTokenCacheTd(), new EventBusPosterTd());
+    }
+
+    private static class LoginHttpEndpointSyncTd implements LoginHttpEndpointSync {
+
+        @Override
+        public EndpointResult loginSync(String username, String password) throws NetworkErrorException {
+            return null;
+        }
+    }
+
+    private static class AuthTokenCacheTd implements AuthTokenCache {
+
+        @Override
+        public void cacheAuthToken(String authToken) {
+
+        }
+
+        @Override
+        public String getAuthToken() {
+            return null;
+        }
+    }
+
+    private static class EventBusPosterTd implements EventBusPoster {
+
+        @Override
+        public void postEvent(Object event) {
+
+        }
+    }
 }
