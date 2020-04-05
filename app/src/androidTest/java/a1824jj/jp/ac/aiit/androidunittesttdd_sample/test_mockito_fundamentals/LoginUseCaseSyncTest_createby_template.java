fa05c644bc78353ce737f5f985691921e71524bd
a1824jj <a1824jj@aiit.ac.jp>
2020-04-05 15:43:35 +0900

    sample of tet class template

---
+++ app/src/androidTest/java/a1824jj/jp/ac/aiit/androidunittesttdd_sample/test_mockito_fundamentals/LoginUseCaseSyncTest_createby_template.java
@@ -0,0 +1,51 @@
+package a1824jj.jp.ac.aiit.androidunittesttdd_sample.test_mockito_fundamentals;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.mockito.*;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.mockito.Mockito.*;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class LoginUseCaseSyncTest_createby_template {
+
+    // region constants
+
+    // endregion constants
+
+    // region helper fields
+
+    // endregion helper fields
+
+    a1824jj.jp.ac.aiit.androidunittesttdd_sample.test_doubles_fundamentals.LoginUseCaseSync SUT;
+
+    @Before
+    public void setup() throws Exception {
+        SUT = new a1824jj.jp.ac.aiit.androidunittesttdd_sample.test_doubles_fundamentals.LoginUseCaseSync();
+
+    }
+
+    @Test
+    public void name() {
+        // Arrange
+        // Act
+        // Assert
+    }
+
+    // region helper methods
+
+    // endregion helper methods
+
+    // region helper classes
+
+    // endregion helper classes
+
+}
