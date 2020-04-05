1397541b86fc7491f74f54ccbc3d4e8fc53714ca
a1824jj <a1824jj@aiit.ac.jp>
2020-04-05 14:36:39 +0900

    ready to mocktio test

--- app/src/androidTest/java/a1824jj/jp/ac/aiit/androidunittesttdd_sample/test_mockito_fundamentals/LoginUseCaseSyncTest.java
+++ app/src/androidTest/java/a1824jj/jp/ac/aiit/androidunittesttdd_sample/test_mockito_fundamentals/LoginUseCaseSyncTest.java
@@ -1,9 +1,20 @@
 package a1824jj.jp.ac.aiit.androidunittesttdd_sample.test_mockito_fundamentals;
 
+import android.accounts.NetworkErrorException;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+
+import java.util.List;
 
 import a1824jj.jp.ac.aiit.androidunittesttdd_sample.test_doubles_fundamentals.*;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.*;
 
 public class LoginUseCaseSyncTest {
 
@@ -13,14 +24,30 @@ public class LoginUseCaseSyncTest {
 
     LoginUseCaseSync SUT;
 
-    @Before
-    public void setup(){
+    LoginHttpEndpointSync mLoginHttpEndpointSyncMock;
+    AuthTokenCache mAuthTokenCacheMock;
+    EventBusPoster mEventBusPosterMock;
 
+    @Before
+    public void setup() throws Exception{
+        mLoginHttpEndpointSyncMock = Mockito.mock(LoginHttpEndpointSync.class);
+        mAuthTokenCacheMock = Mockito.mock(AuthTokenCache.class);
+        mEventBusPosterMock = Mockito.mock(EventBusPoster.class);
+
+        SUT = new LoginUseCaseSync(mLoginHttpEndpointSyncMock, mAuthTokenCacheMock, mEventBusPosterMock);
+
+        when(mLoginHttpEndpointSyncMock.loginSync(any(String.class), any(String.class)))
+                .thenReturn(new LoginHttpEndpointSync.EndpointResult(LoginHttpEndpointSync.EndpointResultStatus.SUCCESS, AUTH_TOKEN));
     }
 
     @Test
-    public void loginSync_success_usernameAndPasswordPassedToEndPoint(){
-
+    public void loginSync_success_usernameAndPasswordPassedToEndPoint() throws NetworkErrorException {
+        ArgumentCaptor<String> ac = ArgumentCaptor.forClass(String.class);
+        SUT.loginSync(USERNAME, PASSWORD);
+        verify(mLoginHttpEndpointSyncMock, times(1)).loginSync(ac.capture(), ac.capture());
+        List<String> captures = ac.getAllValues();
+        assertThat(captures.get(0), is(USERNAME));
+        assertThat(captures.get(1), is(PASSWORD));
     }
 
 
